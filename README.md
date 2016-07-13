e7c18f08d08f5f607e63dea3cc26adf2fde5706e
shameless-inc <shameless_inc@outlook.com>
2016-07-13 22:21:56 +0200

    Added readme Improved layout Removed Bootstrap theme Titles are now sorted

---
+++ README.md
@@ -0,0 +1,9 @@
+# TitleDB++
+**a TitleDB.com frontend**
+
+This is my first project that uses AngularJS (and also my first time using GitHub). It is mostly a playground to experiment with said things and aims to probably provide a nice frontend to http://titledb.com as a by-product.
+
+Here's a small todo-list:
+
+- Add proper user agent string to API requests to TitleDB
+- QR-Code output
--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ var titledbApp = angular.module('titledbApp', []);
 
 titledbApp.controller('TitleListController', function TitleListController($scope, $http) {
   $http.get('https://api.titledb.com/v0').then(function(response){
-    $scope.titles = response.data;
+    $scope.titles = response.data.sort(function(a, b){
+      if(a.name.toUpperCase() < b.name.toUpperCase()) return -1;
+      if(a.name.toUpperCase() > b.name.toUpperCase()) return 1;
+      return 0;
+    });
   });
 });
