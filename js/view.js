080ee51360c0b20ae5dc45e61d288133c16f0e71
shameless-inc <shameless_inc@outlook.com>
2016-08-23 21:49:30 +0200

    Updated Material Kit and added license Added ArriveJS and Truncate Implemented truncation Style improvements

--- js/view.js
+++ js/view.js
@@ -2,9 +2,16 @@ $('document').ready(function(){
   $('body').on("show.bs.collapse", ".qr-collapse", function(event){
     var title = angular.element(event.target).scope().title;
     console.log(event);
-    $(event.target).qrcode({text:'https://api.titledb.com/v0/proxy/' + title.titleid});
+    $(event.target).qrcode({text:'https://api.titledb./v0/proxy/' + title.titleid});
   });
+
   $('body').on("hidden.bs.collapse", ".qr-collapse", function(event){
     $(event.target).empty();
+  });
+
+  $(document).arrive(".title", function() {
+      var $newElem = $(this);
+      $($newElem).children('.content').children('h1, p').truncate();
+      //$($newElem).children().children('[data-toggle="tooltip"]').tooltip(); TODO: Fix tootltips
   });
 });
