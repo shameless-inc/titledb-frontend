bb373d4ded8a0e8e14c43d6fda013b53efc0c4ca
shameless-inc <shameless_inc@outlook.com>
2016-09-15 22:49:17 +0200

    Forgot to save view.js...duh

--- js/view.js
+++ js/view.js
@@ -8,15 +8,4 @@ $('document').ready(function(){
   $('body').on("hidden.bs.collapse", ".qr-collapse", function(event){
     $(event.target).empty();
   });
-
-  $(document).arrive(".title", function() {
-      var $newElem = $(this);
-      /*setTimeout(function(){
-        //$($newElem).children('.content').children('.truncate').truncate();
-        $($newElem).children('.content').children('.truncate').dotdotdot({
-          watch: 'window'
-        });
-      }, 10);*/
-      //$($newElem).children().children('[data-toggle="tooltip"]').tooltip(); TODO: Fix tootltips
-  });
 });
