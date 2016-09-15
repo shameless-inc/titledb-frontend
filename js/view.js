3dc28bce0dd17f0a54cbc6cf904995c5138be90e
shameless-inc <shameless_inc@outlook.com>
2016-09-15 23:14:19 +0200

    Removed logging of QR collapse event

--- js/view.js
+++ js/view.js
@@ -1,7 +1,6 @@
 $('document').ready(function(){
   $('body').on("show.bs.collapse", ".qr-collapse", function(event){
     var title = angular.element(event.target).scope().title;
-    console.log(event);
     $(event.target).qrcode({text:'https://api.titledb.com/v0/proxy/' + title.titleid});
   });
 
